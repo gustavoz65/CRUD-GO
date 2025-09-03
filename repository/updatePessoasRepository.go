31b86b97a87a10a2f1b84ce6fa5304030cc1f592
Gustavo rodrigues <gustavo.katia.rodrigues@gmail.com>
2025-09-02 20:40:10 -0400

    feat; refatoring updatePessoasRpository

--- repository/updatePessoasRepository.go
+++ repository/updatePessoasRepository.go
@@ -1,6 +1,10 @@
 package repository
 
 import (
+	"database/sql"
+	"fmt"
+	"time"
+
 	"github.com/gustavoz65/CRUD-NBS-GO/database"
 	"github.com/gustavoz65/CRUD-NBS-GO/models"
 )
@@ -9,18 +13,64 @@ type UpdatePessoasRepository struct {
 }
 
 func (r *UpdatePessoasRepository) UpdatePessoas(pessoa models.Pessoa) error {
-
 	db := database.ConectarDB()
 	defer db.Close()
 
-	query := `UPDATE pessoas SET 
-          nome = ?, descricao = ?, ativo = ?, altura_metros = ?, 
-          nascimento = ?, cep = ?, atualizado_em = ? 
-          WHERE id = ?`
-	_, err := db.Exec(query, pessoa.Nome, pessoa.Descricao, pessoa.Ativo, pessoa.Altura_metros, pessoa.Nascimento, pessoa.Cep, pessoa.Atualizado_em, pessoa.Id)
-	if err != nil {
-		return err
-	}
-	return nil
+	time.Sleep(10 * time.Second)
 
+	fmt.Println("Tentando com a Procedure")
+
+	err := r.updatePessoaWhithProcedure(db, pessoa)
+	if err == nil {
+		fmt.Println("Procedure executada com sucesso")
+		return nil
+	}
+	fmt.Println("Procedure falhou, tentando com a Query")
+
+	time.Sleep(10 * time.Second)
+
+	err = r.updatePessoaWhithQuery(db, pessoa)
+	if err == nil {
+		fmt.Println("Query executada com sucesso")
+		return nil
+	}
+	return err
+
+}
+
+func (r *UpdatePessoasRepository) updatePessoaWhithProcedure(db *sql.DB, pessoa models.Pessoa) error {
+	query := `CALL sp_atualizar_pessoa (?,?,?,?,?,?,?,?)`
+
+	_, err := db.Exec(
+		query,
+		pessoa.Id,
+		pessoa.Nome,
+		pessoa.Descricao,
+		pessoa.Ativo,
+		pessoa.Altura_metros,
+		pessoa.Nascimento,
+		pessoa.Cep,
+		pessoa.Atualizado_em,
+	)
+	return err
+}
+
+func (r *UpdatePessoasRepository) updatePessoaWhithQuery(db *sql.DB, pessoa models.Pessoa) error {
+	query := `UPDATE pessoas SET 
+	nome = ?,
+	descricao = ?,
+	ativo = ?,
+	altura_metros = ?,
+	`
+	_, err := db.Exec(
+		query,
+		pessoa.Nome,
+		pessoa.Descricao,
+		pessoa.Ativo,
+		pessoa.Altura_metros,
+		pessoa.Nascimento,
+		pessoa.Cep,
+		pessoa.Atualizado_em,
+	)
+	return err
 }
